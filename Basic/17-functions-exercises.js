9639e45f716b1a64fe41b28639d9a78415051a47
Lyss2120 <lys.ivonne@gmail.com>
2026-01-24 18:58:34 -0300

    19 object exercises

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -74,71 +74,73 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
 //es primo si solo se divide por 1 y  por si mismo
 
-function primeNumber(num) {
-    let isPrime = `${num} es primo`
-    for (let i = 2; i < num; i++) {
+// function primeNumber(num) {
+//     let isPrime = `${num} es primo`
+//     for (let i = 2; i < num; i++) {
 
-        num % i == 0 ? (isPrime = num + ' no es primo ', console.log('no es primo, es divisible por ' + i)) : console.log(i);
-    }
-    return isPrime
-}
-console.log(primeNumber(8));
+//         num % i == 0 ? (isPrime = num + ' no es primo ', console.log('no es primo, es divisible por ' + i)) : console.log(i);
+//     }
+//     return isPrime
+// }
+// console.log(primeNumber(8));
 
-// 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-const comunElements = (arr1, arr2) => {
-    let arrCom = []
-    arr1.forEach((element) => {
-        if (arr2.includes(element)) {
-            arrCom.push(element)
-        }
-    })
-    return arrCom
-}
-console.log(comunElements(array1, array2));
+// // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [4, 5, 6, 7, 8];
+// const comunElements = (arr1, arr2) => {
+//     let arrCom = []
+//     arr1.forEach((element) => {
+//         if (arr2.includes(element)) {
+//             arrCom.push(element)
+//         }
+//     })
+//     return arrCom
+// }
+// console.log(comunElements(array1, array2));
 
-// 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
-let arrayPares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-const sumPares = (arr) => {
-    let result = 0;
-    arrayPares.forEach((num) => {
-        if (num % 2 == 0) {
-            // console.log(num);
-            result += num
-        }
-    })
-    return 'los pares suman ' + result
-}
+// // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+// let arrayPares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// const sumPares = (arr) => {
+//     let result = 0;
+//     arrayPares.forEach((num) => {
+//         if (num % 2 == 0) {
+//             // console.log(num);
+//             result += num
+//         }
+//     })
+//     return 'los pares suman ' + result
+// }
 
-console.log(sumPares(arrayPares));
+// console.log(sumPares(arrayPares));
 
-// 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
+// // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
 
-const squareNumber = () => {
-    let arrCuad = []
-    arrayPares.forEach((num) => {
-        arrCuad.push(num ** 2)
-    })
-    return arrCuad
-}
-console.log(squareNumber([1, 2, 3, 4, 5]));
+// const squareNumber = () => {
+//     let arrCuad = []
+//     arrayPares.forEach((num) => {
+//         arrCuad.push(num ** 2)
+//     })
+//     return arrCuad
+// }
+// console.log(squareNumber([1, 2, 3, 4, 5]));
 
-// 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
+// // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
 
-const reverseString = (str) => {
-    let words = str.split(' ').reverse().join(' ')
-    return words
-}
-console.log(reverseString('hola como5 estas'))
-// 10. Crea una función que calcule el factorial de un número dado
-// 
-const factorial = (num) => {
-    let result = num
-    for (let i = num-1; i >= 1; i--) {
-       result = result * (i)
-    }
+// const reverseString = (str) => {
+//     let words = str.split(' ').reverse().join(' ')
+//     return words
+// }
+// console.log(reverseString('hola como estas'))
+// // 10. Crea una función que calcule el factorial de un número dado
+// // 
+// const factorial = (num) => {
+//     let result = num
+//     for (let i = num - 1; i >= 1; i--) {
+//         result = result * (i)
+//     }
 
-    return result
-}
-console.log(factorial(4)) 
+//     return result
+// }
+// console.log(factorial(4))
+
+
--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -5,13 +5,30 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=15675
 
 // 1. Crea un objeto con 3 propiedades
 
+let user = {
+    nickName :'nana',
+    age: 75 ,
+    password : 'jgjgjgj'
+}
+
 // 2. Accede y muestra su valor
+
+console.log(user.nickName);
 
 // 3. Agrega una nueva propiedad
 
+user.name= 'Hortencia'
+
 // 4. Elimina una de las 3 primeras propiedades
 
+delete user.age
+
 // 5. Agrega una función e invócala
+
+user.log = (name) =>{ console.log(`hola mi nombre es ${name}`);
+}
+user.log('Hortencia')
+console.log(user);
 
 // 6. Itera las propiedades del objeto
 
