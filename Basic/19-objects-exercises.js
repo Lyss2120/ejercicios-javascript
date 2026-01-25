b7f7c6113b387fd5ae8ca158ad28a58ad8ebf954
Lyss2120 <lys.ivonne@gmail.com>
2026-01-25 19:20:57 -0300

    19 object exercises.1

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -32,10 +32,33 @@ console.log(user);
 
 // 6. Itera las propiedades del objeto
 
+for (const value in user){
+    console.log(value, user[value],'USER');
+}
+Object.entries(user).forEach(([key, value]) => {
+    console.log(key, value);
+})
 // 7. Crea un objeto anidado
+
+const empleado = {
+    name:'',
+    age: 0,
+    job:{
+        cargo: '',
+        tasks : '',
+        exp: 0
+    }
+}
 
 // 8. Accede y muestra el valor de las propiedades anidadas
 
+console.log(empleado.job, 'propiedades anidadas');
+
 // 9. Comprueba si los dos objetos creados son iguales
 
+console.log(user === empleado);
+
+
 // 10. Comprueba si dos propiedades diferentes son iguales
+
+console.log(user.name=== user.nickName)
