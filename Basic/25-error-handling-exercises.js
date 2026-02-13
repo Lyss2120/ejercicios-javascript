43ef4a689500f19b4ee213b73c11c38f94c78cd4
Lyss2120 <lys.ivonne@gmail.com>
2026-02-13 00:36:41 -0300

    25-error-handling.6

--- Basic/25-error-handling-exercises.js
+++ Basic/25-error-handling-exercises.js
@@ -3,210 +3,224 @@ Clase 41 - Ejercicios: Manejo de errores
 Vídeo: https://youtu.be/1glVfFxj8a4?t=20392
 */
 
-// 1. Captura una excepción utilizando try-catch
+// // 1. Captura una excepción utilizando try-catch
 
-// let age = 55
+// // let age = 55
 
-const ageCheck = () => {
-    try {
-        console.log(age);
-    }
-    catch (err) {
-        console.log('ha ocurrido un error: ', err.message);
-    }
-}
-ageCheck()
-
-// 2. Captura ()una excepción utilizando try-catch y finally
-
-function loquesea() {
-    try {
-        console.log(age);
-    }
-    catch (err) {
-        console.log('ha ocurrido un error en loquesea: ', err.message);
-    }
-    finally {
-        console.log('has ingresado tu edad?');
-    }
-}
-loquesea()
-// 3. Lanza una excepción genérica
-
-const saludo = (firstName) => {
-    if (firstName === '') {
-        throw new Error("no ha ingresado un nombre para saludo")
-    };
-    return firstName
-}
-try {
-    saludo('')
-} catch (error) {
-    console.log(error.message);
-
-}
-const salu2 = (firstName, lastName) => {
-    if (typeof (firstName) !== 'string' || typeof (lastName) !== 'string') {
-        // throw new TypeError("debe ingresar un string")
-        throw new TypeError("salu2 debe ingresar un string")
-    }
-    if (firstName.length >= 10 || lastName.length >= 10) {
-        throw new myError('debe ingresar un nombre y apellido de menos de 10 caracteres', firstName, lastName)
-    }
-    return `Bienvenido ${firstName} ${lastName}`
-}
-
-// try {
-//     console.log(salu2(4,7));
-// } catch (error) {
-//     if (error instanceof TypeError) {
-//         console.log('ha ocurrido un error de tipo:', error.message);
-
+// const ageCheck = () => {
+//     try {
+//         console.log(age);
+//     }
+//     catch (err) {
+//         console.log('ha ocurrido un error: ', err.message);
 //     }
 // }
-// 4. Crea una excepción personalizada
+// ageCheck()
 
-class myError extends Error {
-    constructor(message, firstName, lastName) {
-        super(message)
-        this.firstName = firstName
-        this.lastName = lastName
-    }
-    identifyUser() {
-        console.log('el usuario ingresado es: ', this.firstName, this.lastName);
-    }
-}
+// // 2. Captura ()una excepción utilizando try-catch y finally
 
-// 5. Lanza una excepción personalizada
+// function loquesea() {
+//     try {
+//         console.log(age);
+//     }
+//     catch (err) {
+//         console.log('ha ocurrido un error en loquesea: ', err.message);
+//     }
+//     finally {
+//         console.log('has ingresado tu edad?');
+//     }
+// }
+// loquesea()
+// // 3. Lanza una excepción genérica
 
-try {
-    console.log(salu2('Mario', 'Gonzalez'));
-    console.log(salu2('Marianellaaaa', 'Gonzalezzzzzz'));
+// const saludo = (firstName) => {
+//     if (firstName === '') {
+//         throw new Error("no ha ingresado un nombre para saludo")
+//     };
+//     return firstName
+// }
+// try {
+//     saludo('')
+// } catch (error) {
+//     console.log(error.message);
 
-} catch (error) {
-    console.log('se ha producido un error personalizado: ', error.message);
-    error.identifyUser()
-}
+// }
+// const salu2 = (firstName, lastName) => {
+//     if (typeof (firstName) !== 'string' || typeof (lastName) !== 'string') {
+//         // throw new TypeError("debe ingresar un string")
+//         throw new TypeError("salu2 debe ingresar un string")
+//     }
+//     if (firstName.length >= 10 || lastName.length >= 10) {
+//         throw new myError('debe ingresar un nombre y apellido de menos de 10 caracteres', firstName, lastName)
+//     }
+//     return `Bienvenido ${firstName} ${lastName}`
+// }
 
+// // try {
+// //     console.log(salu2(4,7));
+// // } catch (error) {
+// //     if (error instanceof TypeError) {
+// //         console.log('ha ocurrido un error de tipo:', error.message);
 
-// 6. Lanza varias excepciones según una lógica definida
+// //     }
+// // }
+// // 4. Crea una excepción personalizada
 
-const checkLicense = (license) => {
-    try {
-        if (license === 0) {
-            throw new Error('debe ingresar una licencia superior a 1')
-        }
-        // if (typeof license !== 'number') {
-        //     throw new TypeError('debe ingresar un dato tipo número')
-        // }
-        if (!Number.isInteger(license)) {
-            throw new TypeError('debe ingresar un número entero')
-        }
-        if (license > 4) {
-            throw new RangeError('debe ingresar una licencia entre 1 y 4')
-        }
-        if (!license) {
-            throw new ReferenceError('debe ingresar una licencia')
-        }
-        else {
-            return `licencia tipo ${license} ingresada exitosamente`
-        }
-    }
-    catch (error) {
-        console.log(`se ha producido un ${error.name} en checkLicense: ${error.message}`);
-    }
-}
-console.log(checkLicense('3'))
+// class myError extends Error {
+//     constructor(message, firstName, lastName) {
+//         super(message)
+//         this.firstName = firstName
+//         this.lastName = lastName
+//     }
+//     identifyUser() {
+//         console.log('el usuario ingresado es: ', this.firstName, this.lastName);
+//     }
+// }
 
+// // 5. Lanza una excepción personalizada
 
-// 7. Captura varias excepciones en un mismo try-catch
+// try {
+//     console.log(salu2('Mario', 'Gonzalez'));
+//     console.log(salu2('Marianellaaaa', 'Gonzalezzzzzz'));
 
-function mult(num, num2) {
-    if (!Number.isInteger(num) || !Number.isInteger(num2)) {
-        throw new TypeError('debe ingresar números enteros')
-    }
-    if (num === 0 || num2 === 0) {
-        throw new RangeError('la operación mult sólo recibe números mayores a 0')
-    } else return num * num2
-}
-
-try {
-    console.log(mult(5, 5))
-    console.log(mult(0, 0))
-    console.log(mult(5.5, 5))
-} catch (error) {
-    if (error instanceof RangeError) {
-        console.log(`se ha producido un ${error.name} en mult: ${error.message}`);
-    }
-    if (error instanceof TypeError) {
-        console.log(`se ha producido un error ${error.name} en mult: ${error.message}`);
-    }
-    // if (error instanceof error) {
-    //     console.log(`se ha producido un error ${error.name} en mult: ${error.message}`);
-    // }
-}
+// } catch (error) {
+//     console.log('se ha producido un error personalizado: ', error.message);
+//     error.identifyUser()
+// }
 
 
-// 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
-let numbers = [1,2,3]
-const transformToFloat = (arr) => {
-    // console.log(transformToFloat);
-    arr.forEach(element => {
-        if (typeof element !== 'number') {
-            throw new TypeError('debe ingresar datos tipo number')
-        }
-        if (element === '') {
-            throw new TypeError('debe ingresar datos no vacios')
-        } 
-        else {
-            console.log(element.toFixed(1));
-            arr.splice(arr.indexOf(element), 1, element.toFixed(1))
-        }
-    }); console.log(arr);
-    
-}
-try {
-    console.log(transformToFloat(numbers));
-}
-catch (error) {
-    console.log(`se ha producido un error ${error.name} en transformToFloat: ${error.message}`);
-}
+// // 6. Lanza varias excepciones según una lógica definida
 
-// 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
+// const checkLicense = (license) => {
+//     try {
+//         if (license === 0) {
+//             throw new Error('debe ingresar una licencia superior a 1')
+//         }
+//         // if (typeof license !== 'number') {
+//         //     throw new TypeError('debe ingresar un dato tipo número')
+//         // }
+//         if (!Number.isInteger(license)) {
+//             throw new TypeError('debe ingresar un número entero')
+//         }
+//         if (license > 4) {
+//             throw new RangeError('debe ingresar una licencia entre 1 y 4')
+//         }
+//         if (!license) {
+//             throw new ReferenceError('debe ingresar una licencia')
+//         }
+//         else {
+//             return `licencia tipo ${license} ingresada exitosamente`
+//         }
+//     }
+//     catch (error) {
+//         console.log(`se ha producido un ${error.name} en checkLicense: ${error.message}`);
+//     }
+// }
+// console.log(checkLicense('3'))
 
-const myObj = { name: 'mario', lastName: 'gonzalez' }
-const checkProp = (obj) => {
-    if (obj.age) {
-        return obj.age
-    } else { throw new noPropError('No ha ingresado la edad del usuario') }
 
-}
+// // 7. Captura varias excepciones en un mismo try-catch
 
-class noPropError extends Error {
-    constructor(message) {
-        super(message)
-    }
-}
+// function mult(num, num2) {
+//     if (!Number.isInteger(num) || !Number.isInteger(num2)) {
+//         throw new TypeError('debe ingresar números enteros')
+//     }
+//     if (num === 0 || num2 === 0) {
+//         throw new RangeError('la operación mult sólo recibe números mayores a 0')
+//     } else return num * num2
+// }
 
-try { checkProp(myObj) }
-catch (err) {
-    console.log('error personalizado:', err.message);
-}
+// try {
+//     console.log(mult(5, 5))
+//     console.log(mult(0, 0))
+//     console.log(mult(5.5, 5))
+// } catch (error) {
+//     if (error instanceof RangeError) {
+//         console.log(`se ha producido un ${error.name} en mult: ${error.message}`);
+//     }
+//     if (error instanceof TypeError) {
+//         console.log(`se ha producido un error ${error.name} en mult: ${error.message}`);
+//     }
+//     // if (error instanceof error) {
+//     //     console.log(`se ha producido un error ${error.name} en mult: ${error.message}`);
+//     // }
+// }
+
+
+// // 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
+// let numbers = [1,2,3]
+// const transformToFloat = (arr) => {
+//     // console.log(transformToFloat);
+//     arr.forEach(element => {
+//         if (typeof element !== 'number') {
+//             throw new TypeError('debe ingresar datos tipo number')
+//         }
+//         if (element === '') {
+//             throw new TypeError('debe ingresar datos no vacios')
+//         } 
+//         else {
+//             arr.splice(arr.indexOf(element), 1, element.toFixed(1))
+//         }
+//     }); return arr
+
+// }
+// try {
+//     console.log(transformToFloat(numbers));
+//     // console.log(transformToFloat(['1', '2', '3']));
+//     console.log(transformToFloat([1.5, 2.0, 2.66]));
+// }
+// catch (error) {
+//     console.log(`se ha producido un error ${error.name} en transformToFloat: ${error.message}`);
+// }
+
+// // 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
+
+// const myObj = { name: 'mario', lastName: 'gonzalez' }
+// const checkProp = (obj) => {
+//     if (obj.age) {
+//         return obj.age
+//     } else { throw new noPropError('No ha ingresado la edad del usuario') }
+
+// }
+
+// class noPropError extends Error {
+//     constructor(message) {
+//         super(message)
+//     }
+// }
+
+// try { checkProp(myObj) }
+// catch (err) {
+//     console.log('error personalizado:', err.message);
+// }
 
 // 10. Crea una función que realice reintentos en caso de error hasta un máximo de 10
-const getPerson = (data) => {
-    try {
-        console.log(data.title);
-    } catch (error) {
-        console.log(`se ha producido un error ${error.name} en getPerson: ${error.message}`);
-    }
+const getPerson = async (url) => {
+    let count = 0;
+    let person = null
+    const fetchPerson = async  (url) => {
+        count++;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.log(`se ha producido un error ${error.name} en getPerson: ${error.message}`);
+            if (count <= 10) {
+               return await fetchPerson(url)
+            } else {
+                console.log('se ha excedido el numero de reintentos');
+                return null
+            }
+        }
+    };
+    person = await fetchPerson(url) //aca para dejar que se obtengan los datos antes de setear la persona
+    return `la persona es: ${JSON.stringify(person)}`
 }
-const fetchPerson = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => getPerson(data)
-            //   , getPerson(data)
-        )
-}
-fetchPerson('https://jsonplaceholder.typicode.com/todos/1')
+ // Para probarlo (ya que getPerson devuelve una promesa):
+getPerson('https://jsonplaceholder.typicode.com/todos/1')
+    .then(resultado => console.log(resultado));
+
 
